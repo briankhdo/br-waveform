dd74bd78fb71aef15d090ec5c50d8f527e30f331
Brian Doan <quangkhoidoan@gmail.com>
2015-08-29 20:24:26 +0700

    Convert audio file to wav

--- lib/br_waveform.rb
+++ lib/br_waveform.rb
@@ -2,7 +2,13 @@ class BrWaveForm
 private
 	# Convert audio file to wav for reading
 	def convert_to_wav
-
+		# check if file exists?
+		if File.exist? @filename
+			`ffmpeg -y -i "#{@filename}" -f wav "#{@filename_without_extension}.wav"`
+			File.delete(@filename)
+		else
+			raise IOError, "File #{@filename} not found"
+		end
 	end
 
 	# read wave file
@@ -12,6 +18,7 @@ private
 public
 	def initialize filename
 		@filename = filename
+		@filename_without_extension = File.basename(@filename, File.extname(@filename))
 	end
 
 	# generate waveform png
