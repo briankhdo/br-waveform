d40264095e5cdbaa370f84fcdf115d0c7d8ac9d4
Brian Doan <quangkhoidoan@gmail.com>
2015-08-29 20:43:35 +0700

    Run ffmpeg without output

--- lib/br_waveform.rb
+++ lib/br_waveform.rb
@@ -4,7 +4,7 @@ private
 	def convert_to_wav
 		# check if file exists?
 		if File.exist? @filename
-			`ffmpeg -y -i "#{@filename}" -f wav "#{@filename_without_extension}.wav"`
+			fork { exec "ffmpeg -y -i \"#{@filename}\" -f wav \"#{@filename_without_extension}.wav\"" }
 			File.delete(@filename)
 		else
 			raise IOError, "File #{@filename} not found"
@@ -76,5 +76,59 @@ public
 		data = spectrum_data numberOfBar
 		# generate png
 
+		array = array[0..(numberOfBar-1)]
+		# clean up files
+		puts "[ProcessVideos] cleaning up files #{filename}, #{filename}.wav"
+		File.delete(filename + ".wav")
+		puts "[ProcessVideos] done cleaning up files"
+
+		max = array.max
+		width = numberOfBar * 4 + (numberOfBar - 1) * 2
+		png = ChunkyPNG::Image.new(width, 56, ChunkyPNG::Color::TRANSPARENT)
+		x = 0
+		array.each do |d|
+			# drawing 4px bar
+			# calc start y
+			(1..4).each do |i|
+				maxY = ((d / max) * 54).to_i + 1
+				y = -1
+				until y == maxY
+					y += 1
+					if y < maxY - 2
+						png[x, y] = ChunkyPNG::Color::rgba(255, 255, 255, 255)
+					end
+					# draw circle
+					if y == maxY - 2
+						if i == 1 || i == 4
+							png[x, y] = ChunkyPNG::Color::rgba(255, 255, 255, 247)
+						else
+							png[x, y] = ChunkyPNG::Color::rgba(255, 255, 255, 255)
+						end
+					end
+					if y == maxY - 1
+						if i == 1 || i == 4
+							# transparent level 1
+							png[x, y] = ChunkyPNG::Color::rgba(254, 254, 254, 158)
+						else
+							png[x, y] = ChunkyPNG::Color::rgba(255, 255, 255, 255)
+						end
+					end
+					if y == maxY
+						if i == 2 || i == 3
+							# transparent level 2
+							png[x, y] = ChunkyPNG::Color::rgba(253, 253, 253, 79)
+						end
+					end
+				end
+				# move to next column
+				x += 1
+			end
+			# move to next column with spacing
+			x += 2
+		end
+		pngFilename = "waveform-#{youtubeId}-#{Time.now.to_i}.png"
+		png.save(pngFilename, :interlace => true)
+
+		puts 
 	end
 end
