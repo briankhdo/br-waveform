f85ec85cd8c6a0e7f9f9da037f8941c9c6d33cbc
Brian Doan <quangkhoidoan@gmail.com>
2015-08-29 20:38:49 +0700

    Convert audio to wav, read wav file to have humbers

--- lib/br_waveform.rb
+++ lib/br_waveform.rb
@@ -14,6 +14,54 @@ private
 	# read wave file
 	def read_wav
 
+		convert_to_wav
+
+		gem 'wav-file'
+		require 'wav-file'
+
+		# read wavefile
+		f = open("#{@filename_without_extension}.wav")
+		@format = WavFile::readFormat(f)
+
+		dataChunk = WavFile::readDataChunk(f)
+		f.close
+		bit = 's*' if @format.bitPerSample == 16 # int16_t
+		bit = 'c*' if @format.bitPerSample == 8 # signed char
+		wavs = dataChunk.data.unpack(bit) # read binary
+	end
+
+	def spectrum_data numberOfBar
+
+		wavs = read_wav
+
+		@spectrum_array = []
+
+		samples = (wavs.length / numberOfBar).to_i
+		puts @format
+		i = 0
+		until i > wavs.length
+			istart = i
+			iend = i + samples
+			pos = []
+			wavs[istart..iend].each do |v|
+				pos.push(v.abs)
+			end
+			avg = 0
+			unless pos.length == 0
+				avg = (pos.inject{ |sum, el| sum + el }.to_f / pos.size.to_f) / 32768.0
+			else
+				break
+			end
+
+			# increase
+			avg *= 1.2
+
+			avg = 1 if avg > 1
+			@spectrum_array.push(avg)
+			i += samples
+		end
+
+		@spectrum_array
 	end
 public
 	def initialize filename
@@ -22,7 +70,9 @@ public
 	end
 
 	# generate waveform png
-	def generate filename
-
+	def generate filename, numberOfBar: 100
+		data = spectrum_data numberOfBar
+		puts data
+		# generate png
 	end
 end
