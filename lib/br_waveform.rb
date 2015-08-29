ac7bd5b0da6dce101bbf5959cd28b27c14b93ea7
Brian Doan <quangkhoidoan@gmail.com>
2015-08-29 20:19:15 +0700

    Init class and structure

---
+++ lib/br_waveform.rb
@@ -0,0 +1,21 @@
+class BrWaveForm
+private
+	# Convert audio file to wav for reading
+	def convert_to_wav
+
+	end
+
+	# read wave file
+	def read_wav
+
+	end
+public
+	def initialize filename
+		@filename = filename
+	end
+
+	# generate waveform png
+	def generate filename
+
+	end
+end
